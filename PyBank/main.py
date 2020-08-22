2f3f9993f4c3f42510fcc9d7ad09a25c85f4c926
ashnid <68619608+ashnid@users.noreply.github.com>
2020-08-22 19:58:14 -0400

    master

---
+++ PyBank/main.py
@@ -0,0 +1,115 @@
+#Print top label
+print("Financial Analysis")
+print("----------------------------")
+
+import os
+import csv
+
+#Set path for file
+csvpath = os.path.join(".", "Resources", "budget_data.csv") 
+pnl = 0.0
+pnl_list = []
+last_change = 0.0
+i = 0
+
+# Open CSV file
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile,delimiter=",")
+
+    next(csvreader) #Skip the header row
+
+    count = sum(1 for row in csvreader)
+
+    #The total number of months included in the dataset
+    print("Total Months: " + str(count)) # Row count minus header
+
+    csvfile.seek(0) # back to the begining of file
+    next(csvreader) #Skip the header row
+
+    for row in csvreader:
+        #print(row[1])
+        pnl = pnl + float(row[1])
+        if i == 0:
+            last_change = float(row[1])
+        else:
+            pnl_list.append(float(row[1]) - last_change)
+            last_change = float(row[1])
+        i += 1
+
+    #The average of the changes in "Profit/Losses" over the entire period
+    print("Total Profit and Losses: " + str(pnl))
+
+    def Average(lst): 
+        return sum(lst) / len(lst)
+
+    print("Average Change in Profit and Losses: " + str(Average(pnl_list)))
+
+    #print(pnl_list)
+
+    #The greatest increase in profits (date and amount) over the entire period
+
+    # sorting the list 
+    sorted(pnl_list)
+    #print(sorted(pnl_list))
+
+    # printing the last element 
+    #print("Greatest Increase in Profits:", sorted(pnl_list)[-1]) 
+
+    #print(sorted(pnl_list)[-1])
+    largest_number = sorted(pnl_list)[-1]
+    #print(largest_number)
+
+    #Find index of largest number in budget_data file
+    largest_number_index = pnl_list.index(largest_number)
+    #print("Largest Number Index:", largest_number_index)
+    
+    #Print date of largest increase in profits
+    
+    #sorting the list 
+    sorted(pnl_list)
+    #print(sorted(pnl_list))
+
+    #printing the first element 
+    #print("Greatest Decrease in Profits:", min(pnl_list))
+    smallest_number = min(pnl_list)
+    #print(smallest_number)
+
+    smallest_number_index= pnl_list.index(smallest_number)
+    #print("Smallest Number Index:", smallest_number_index)
+
+    csvfile.seek(0) # back to the begining of file
+    next(csvreader) #Skip the header row
+
+    max_date = ""
+    min_date = ""
+
+    i = 0
+    for row in csvreader:
+        #print(row[1])
+        if i == (largest_number_index + 1): # Index +1 since we are looking for the change from the previous year to the next year
+            #print(row[0])
+            max_date = row [0]
+        elif i == (smallest_number_index + 1):
+            #print(row[0])
+            min_date = row [0]
+        i += 1
+        
+    print("Greatest Increase in Profits:", sorted(pnl_list)[-1], max_date)
+    print("Greatest Decrease in Profits:", min(pnl_list), min_date)
+
+    #Export a text file with the results
+    path = os.path.join('analysis', 'budget_results.txt')
+    with open(path, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(["Financial Analysis"])
+        csvwriter.writerow(["----------------------------------------"])
+        csvwriter.writerow(f"Total Months: {count} ")
+        csvwriter.writerow(f"Total: {pnl} ")
+        csvwriter.writerow(f"Average Change: {Average(pnl_list)} ")
+        csvwriter.writerow(f"Greatest Increase in Profits: {max_date} {largest_number} ")
+        csvwriter.writerow(f"Greatest Decrease in Profits: {min_date} {smallest_number} ")
+            
+
+
+#csvfile.close()
---
+++ PyPoll/main.py
@@ -0,0 +1,99 @@
+#Print top label
+print("Election Results")
+print("----------------------------")
+
+import os
+import csv
+from collections import Counter
+
+#Set path for file
+csvpath = os.path.join(".", "Resources", "election_data.csv") 
+
+
+# Open CSV file
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile,delimiter=",")
+
+    next(csvreader) #Skip the header row
+
+    count = sum(1 for row in csvreader)
+
+    #The total number of months included in the dataset
+    print("Total Votes: " + str(count)) # Row count minus header
+    print("----------------------------")
+
+    #complete list of candidates who received votes
+    csvfile.seek(0) # back to the begining of file
+    next(csvreader) #Skip the header row
+    
+    #Create blank list for candidate names
+    candidates_list = []
+    for row in csvreader:
+        if row[2] not in candidates_list:
+            candidates_list.append(row[2])
+    print ("Candidates who recieved votes" ,candidates_list)
+    print("----------------------------")
+
+    #The total number of votes each candidate won
+    csvfile.seek(0) #back to the begining of file
+    next(csvreader) #Skip the header row
+
+    #variables
+    candidates_unique = []
+    candidate_count = []
+
+    for row in csvreader:
+            #if candidate is alreaady in list then locate the candidate by index # and increment the vote count by 1
+            if row[2] in candidates_unique:
+                candidate_index = candidates_unique.index(row[2])
+                candidate_count[candidate_index] = candidate_count[candidate_index] + 1
+            else:
+                #if candidate was not found in candidates_unique list then add to list and add 1 to vote count
+                candidates_unique.append(row[2])
+                candidate_count.append(1)
+    print ("Number of votes for each candidate:", candidate_count)
+
+    #The percentage of votes each candidate won
+    Khan_count = (candidate_count [0] /count) *100
+    Correy_count = (candidate_count [1]/count) *100
+    Li_count = (candidate_count [2]/count) *100
+    OTooley_count = (candidate_count [3]/count) *100
+    print("Percentage for Khan:", Khan_count)
+    print("Percentage for Correy:", Correy_count)
+    print("Percentage for Li:" , Li_count)
+    print("Percentage for O'Tooley:" , OTooley_count)
+
+    #The winner of the election based on popular vote.
+    pct = []
+    max_votes = candidate_count[0]
+    max_index = 0
+
+    for x in range(len(candidates_unique)):
+        vote_pct = round(candidate_count[x]/count*100, 2)
+        pct.append(vote_pct)
+        
+        if candidate_count[x] > max_votes:
+            max_votes = candidate_count[x]
+            max_index = x
+    winner = candidates_unique[max_index] 
+
+    print("----------------------------")
+    print ("Winner:" , winner)
+    print("----------------------------")
+
+
+# Export a text file with the results
+output_path = os.path.join('analysis', 'election_results.txt')
+with open(output_path, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(["Election Results"])
+    csvwriter.writerow(["----------------------------------------"])
+    csvwriter.writerow(["Total Votes: " + str(count)])
+    csvwriter.writerow(["----------------------------------------"])
+    csvwriter.writerow(["Number of votes for each candidate:", candidate_count])
+    csvwriter.writerow(["----------------------------------------"])
+    csvwriter.writerow(["Percentage for Khan:", Khan_count, "Percentage for Correy:", Correy_count, "Percentage for Li:" , Li_count, "Percentage for O'Tooley:" , OTooley_count])
+    csvwriter.writerow(["----------------------------------------"])
+    csvwriter.writerow(["Winner:" , winner])
+    csvwriter.writerow(["----------------------------------------"])
